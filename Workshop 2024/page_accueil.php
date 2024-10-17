5b703225b19df559fb7d5c341eb914e56e60e7ea
MSPaulR31 <134699948+MSPaulR31@users.noreply.github.com>
2024-10-17 11:22:22 +0200

    Update page_accueil.php

--- Workshop 2024/page_accueil.php
+++ Workshop 2024/page_accueil.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="modera.png">
 </head>
 <body>
 
