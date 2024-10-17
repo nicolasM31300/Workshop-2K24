aaf14f416ad4d312bf9da84379b461f14ce13f3e
MSPaulR31 <134699948+MSPaulR31@users.noreply.github.com>
2024-10-17 11:23:24 +0200

    Update page_educatif.php

--- Workshop 2024/page_educatif.php
+++ Workshop 2024/page_educatif.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Éducation</title>
     <link rel="stylesheet" href="education.css">
+    <link rel="icon" type="image/png" href="modera.png">
 </head>
 <body>
     <div class="iphone">
